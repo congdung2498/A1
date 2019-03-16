c83b15f6edbeb868e6879b36800924d953dff0e6
CongDung <congdung2498@gmail.com>
2019-03-16 12:57:19 +0700

    aa

---
+++ app/src/main/java/com/example/nguyentuan/tracnghiem1/monhoc/HomeFragment.java
@@ -0,0 +1,55 @@
+package com.example.nguyentuan.tracnghiem1.monhoc;
+
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.example.nguyentuan.tracnghiem1.MainActivity;
+import com.example.nguyentuan.tracnghiem1.R;
+import com.example.nguyentuan.tracnghiem1.score.ScoreAdapter;
+import com.example.nguyentuan.tracnghiem1.score.ScoreController;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+    private ImageButton btnA1;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Home");
+        return inflater.inflate(R.layout.fragment_home, container, false);
+
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        btnA1 = (ImageButton) getActivity().findViewById(R.id.btnA1);
+        btnA1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                A1Fragment a1Fragment = new A1Fragment();
+                FragmentManager manager = getFragmentManager();
+                manager.beginTransaction().replace(R.id.content_main, a1Fragment, a1Fragment.getTag()).commit();
+            }
+        });
+    }
+
+}
