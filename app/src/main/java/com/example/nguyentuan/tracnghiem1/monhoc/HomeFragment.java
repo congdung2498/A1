29eb3a94cf7c820241ea09f2bf41298d7d32a9fe
Cong Chinh <chinhpc62@wru.vn>
2019-03-19 17:40:39 +0700

    trang chu

--- app/src/main/java/com/example/nguyentuan/tracnghiem1/monhoc/HomeFragment.java
+++ app/src/main/java/com/example/nguyentuan/tracnghiem1/monhoc/HomeFragment.java
@@ -9,10 +9,12 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ViewFlipper;
 
 import com.example.nguyentuan.tracnghiem1.MainActivity;
 import com.example.nguyentuan.tracnghiem1.R;
@@ -23,7 +25,8 @@ import com.example.nguyentuan.tracnghiem1.score.ScoreController;
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-    private ImageButton btnA1;
+    private Button btnA1;
+    ViewFlipper viewFlipper;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -34,14 +37,14 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Home");
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Trang chủ");
         return inflater.inflate(R.layout.fragment_home, container, false);
 
     }
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        btnA1 = (ImageButton) getActivity().findViewById(R.id.btnA1);
+        btnA1 = (Button) getActivity().findViewById(R.id.btnA1);
         btnA1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,6 +53,9 @@ public class HomeFragment extends Fragment {
                 manager.beginTransaction().replace(R.id.content_main, a1Fragment, a1Fragment.getTag()).commit();
             }
         });
+        viewFlipper=(ViewFlipper)getActivity().findViewById(R.id.viewFlipper);
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
     }
 
 }
